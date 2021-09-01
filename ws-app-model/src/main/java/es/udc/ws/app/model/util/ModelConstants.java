abb7da1e06914e6eebca4966d53da3d1ff684b89
Juan Raposo Santiago <juan.raposo@udc.es>
2021-09-01 13:25:30 +0200

    Cambiado nombre de la constante que tiene como valor el nombre del DataSource

--- ws-app-model/src/main/java/es/udc/ws/app/model/util/ModelConstants.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/util/ModelConstants.java
@@ -2,6 +2,6 @@ package es.udc.ws.app.model.util;
 
 public class ModelConstants {
 
-    public static final String MOVIE_DATA_SOURCE = "ws-javaexamples-ds";
+    public static final String APP_DATA_SOURCE = "ws-javaexamples-ds";
 
 }
